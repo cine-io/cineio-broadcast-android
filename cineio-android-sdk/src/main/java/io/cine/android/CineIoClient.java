839035149be511879df5ea6abad31104299b4bf5
Thomas Shafer <thomasjshafer@gmail.com>
2014-09-08 16:23:47 -0700

    publish 0.0.8-SNAPSHOT

--- cineio-android-sdk/src/main/java/io/cine/android/CineIoClient.java
+++ cineio-android-sdk/src/main/java/io/cine/android/CineIoClient.java
@@ -29,7 +29,7 @@ import io.cine.android.api.StreamsResponseHandler;
 
 public class CineIoClient {
 
-    private final String VERSION = "0.0.7";
+    private final String VERSION = "0.0.8-SNAPSHOT";
     private final static String TAG = "CineIoClient";
     private final static String BASE_URL = "https://www.cine.io/api/1/-";
     private final AsyncHttpClient mClient;
